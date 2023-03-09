c520ac380eb98a9928b9164e18713ffc59ca4144
wjiajian <2321781688@qq.com>
2023-03-09 17:31:18 +0800

    5555

--- Decision Tree/DecisionTree.java
+++ Decision Tree/DecisionTree.java
@@ -1,5 +1,6 @@
 public class DecisionTree{
     public static void main(String args[]) {
+        System.out.println(233333);
         
     }
 }
---
+++ Decision Tree/DecisionTree.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+# 读取数据
+Iris = pd.read_csv(r'D:\\jiajian\\code\\GitHub&Study\\The-second-volume-of-junior-year\\Decision Tree\\iris.data', header=None)
+print(Iris)
+
